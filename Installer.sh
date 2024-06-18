a441fef4bdef500c4fa1590cf7cc051791d77f78
Deeznutsdragon <112670665+Deeznutsdragon@users.noreply.github.com>
2024-06-17 20:19:59 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,4 +1,14 @@
- #!/bin/bash
+#!/bin/bash
 
 echo "Work in progress"
-xdg-open "https://discord.gg/N8Hs4pyR"
+discord_link="https://discord.com/your-link-here"
+
+# Check the operating system to determine the command to open the link
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    xdg-open "$discord_link"  # For Linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open "$discord_link"  # For macOS
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
