2f91f2d635231ff8606d91ac429d74822a9f4f60
Deeznutsdragon <112670665+Deeznutsdragon@users.noreply.github.com>
2024-06-24 13:23:20 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,50 +1,6 @@
-#!/bin/bash
 
-main() {
-    echo -e "Welcome to the Installer Script!"
-    echo -e "Please choose an option:"
-    echo -e "1. Join our Discord server and proceed"
-    echo -e "2. Just proceed"
-    read -p "Enter your choice (1 or 2): " choice
-
-    case $choice in
-        1)
-            echo -e "Please join our Discord server at: https://discord.gg/xrf33AvS"
-            echo -e "Press any key to continue..."
-            read -n 1 -s
-            proceed
-            ;;
-        2)
-            proceed
-            ;;
-        *)
-            echo -e "Invalid choice. Exiting..."
-            exit 1
-            ;;
-    esac
-}
-
-proceed() {
-    echo -e "Proceeding with the (dummy) installation process..."
-    echo -e "Step 1: (Dummy) Downloading Latest Application"
-    sleep 2  # Simulate download time
-
-    echo -e "Step 2: (Dummy) Installing Latest Application"
-    sleep 2  # Simulate installation time
-
-    echo -e "Step 3: (Dummy) Downloading YourPatch"
-    sleep 2  # Simulate download time
-
-    echo -e "Step 4: (Dummy) Installing YourPatch"
-    sleep 2  # Simulate installation time
-
-    echo -e "Step 5: (Dummy) Patching Application"
-    sleep 2  # Simulate patching time
-
-    echo -e "Step 6: (Dummy) Installing YourPatch App"
-    sleep 2  # Simulate installation time
-
-    echo -e "Install Complete! Developed by The Leper!"
-}
-
-main
+echo -e "Patching Roblox"
+mv ./sky.dylib "/Applications/Roblox.app/Contents/MacOS/sky.dylib"
+./insert_dylib "/Applications/Roblox.app/Contents/MacOS/sky.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+rm ./insert_dylib
